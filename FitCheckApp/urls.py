ffaf42c282aa61521d73fabf905f342ff7af59a3
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2025-03-07 22:25:53 +0800

    Updated urls.py project level to handle user_login feature

--- FitCheckApp/urls.py
+++ FitCheckApp/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 from UserProfile import views  # Import views from your app
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                 # Admin page
-    path('', views.user_create, name='home'),        # Default route redirects to user_create view
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('user_create/', views.user_create, name='user_create'),
+    path('user_login/', views.user_login, name='user_login'),
     path('health_profile/', views.health_profile, name='health_profile'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
