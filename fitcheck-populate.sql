fc79f5b16cf08025f2950c68f44988f655ac87ac
PaoloOng <paolo.ong@student.ateneo.edu>
2025-05-06 14:24:16 +0800

    adjustments to populate values

--- fitcheck-populate.sql
+++ fitcheck-populate.sql
@@ -9,8 +9,8 @@ TRUNCATE TABLE "NutritionGuidance_meal" CASCADE;
 -- Allergen
 
 INSERT INTO "NutritionGuidance_allergen" (allergen_name) VALUES ('Milk');
-INSERT INTO "NutritionGuidance_allergen" (allergen_name) VALUES ('Sesame');
-INSERT INTO "NutritionGuidance_allergen" (allergen_name) VALUES ('Nuts');
+INSERT INTO "NutritionGuidance_allergen" (allergen_name) VALUES ('Soy');
+INSERT INTO "NutritionGuidance_allergen" (allergen_name) VALUES ('Tree Nuts');
 
 -- Ingredient
 
@@ -32,21 +32,21 @@ INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, 
 INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, recipe_id_id) VALUES ('2 lbs', (SELECT ingredient_id FROM "NutritionGuidance_ingredient" WHERE ingredient_name = 'Ground Beef'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'i walk to burger king'));
 INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, recipe_id_id) VALUES ('3 pcs.', (SELECT ingredient_id FROM "NutritionGuidance_ingredient" WHERE ingredient_name = 'Cashew Nuts'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'then i walk back home from burger king'));
 INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, recipe_id_id) VALUES ('5 cups', (SELECT ingredient_id FROM "NutritionGuidance_ingredient" WHERE ingredient_name = 'White Rice'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'i walk to burger king'));
-INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, recipe_id_id) VALUES ('50 mL', (SELECT ingredient_id FROM "NutritionGuidance_ingredient" WHERE ingredient_name = 'White Rice'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
+INSERT INTO "NutritionGuidance_recipe_ingredients" (quantity, ingredient_id_id, recipe_id_id) VALUES ('3 cups mL', (SELECT ingredient_id FROM "NutritionGuidance_ingredient" WHERE ingredient_name = 'White Rice'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 
 -- Meal
 
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, allergen_id_id, recipe_id_id) VALUES ('4 Cheese Pepperoni Pizza', 1000, (SELECT allergen_id FROM "NutritionGuidance_allergen" WHERE allergen_name = 'Milk'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'i cooka da pizza'));
-INSERT INTO "NutritionGuidance_meal" (meal_name, calories, allergen_id_id, recipe_id_id) VALUES ('Nutty Chocolate', 200, (SELECT allergen_id FROM "NutritionGuidance_allergen" WHERE allergen_name = 'Nuts'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'then i walk back home from burger king'));
-INSERT INTO "NutritionGuidance_meal" (meal_name, calories, allergen_id_id, recipe_id_id) VALUES ('Korean Beef Bowl', 300, (SELECT allergen_id FROM "NutritionGuidance_allergen" WHERE allergen_name = 'Sesame'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'i walk to burger king'));
+INSERT INTO "NutritionGuidance_meal" (meal_name, calories, allergen_id_id, recipe_id_id) VALUES ('Nutty Chocolate', 200, (SELECT allergen_id FROM "NutritionGuidance_allergen" WHERE allergen_name = 'Tree Nuts'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'then i walk back home from burger king'));
+INSERT INTO "NutritionGuidance_meal" (meal_name, calories, allergen_id_id, recipe_id_id) VALUES ('Korean Beef Bowl', 300, (SELECT allergen_id FROM "NutritionGuidance_allergen" WHERE allergen_name = 'Soy'), (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'i walk to burger king'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 1', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 2', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
-INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 3', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
+INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 3', 800, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 4', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 5', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 6', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 7', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
-INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 8', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
+INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 8', 1000, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 9', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 10', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
 INSERT INTO "NutritionGuidance_meal" (meal_name, calories, recipe_id_id) VALUES ('Filler Meal 11', 200, (SELECT recipe_id FROM "NutritionGuidance_recipe" WHERE recipe_instructions = 'boo! gotcha didnt i?'));
